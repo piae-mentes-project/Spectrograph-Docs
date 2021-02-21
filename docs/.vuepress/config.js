51bc90e1aee290e4dbc6e419da18281be01a3342
Liu Baolin--CN <61337149+lzhbhlrPython@users.noreply.github.com>
2021-02-21 09:31:52 +0800

    Update pw

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,22 +1,23 @@
 module.exports = {
-    title: '制谱器开发文档 Offical',
-    description: 'Spectrometer development documents of piae mentes.',
+    title: '开发文档(10min)Offical',
+    description: 'Development documents of Ten Minutes Game Studio..',
     head: [
-        ["link", { rel: "icon", href: "https://storage.pyliubaolin.top/ArchiveImage/2021-02-07_lzhbhlrPython/2021-02-07_19-33-06[gui].jpg" }]
+        ["link", { rel: "icon", href: "./images/hero.jpg" }]
     ],
     plugin: [
         "@vuepress/active-header-links", "@vuepress/back-to-top"
     ],
     themeConfig: {
-        logo: "https://storage.pyliubaolin.top/ArchiveImage/2021-02-07_lzhbhlrPython/2021-02-07_19-33-06[gui].jpg",
+        logo: "./images/hero.jpg",
         nav: [{
             text: "开发文档",
             link: "/dev-doc/"
         }],
         sidebar: {
-            "/projects/": [
-                ["", "项目清单"],
-                ["PublicWelfare_lzhbhlrpython.html", "编码公益网站_Lzhbhlrpython"]
+            "/dev-doc/": [
+                ["", "项目简述"],
+                ["piae-mentes.html", "piae-mentes"],
+                ["spectrograph.html", "Spectrograph"]
             ]
         }
     },
--- docs/index.md
+++ docs/index.md
@@ -1,6 +1,6 @@
 ---
 home: true
-heroImage: https://storage.pyliubaolin.top/ArchiveImage/2021-02-07_lzhbhlrPython/2021-02-07_19-33-06[gui].jpg
+heroImage: ./images/hero.jpg
 actionText: 查看文档 →
 actionLink: /dev-doc/
 footer: BSD-3 Licensed | Copyright © 2021 piae-mentes-project
